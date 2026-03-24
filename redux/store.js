0b2c75097b6502d221e236197d7f8aed262b1f19
ravikant03 <ravikantmishra4568@gmail.com>
2026-03-24 10:18:18 +0530

    used redux-persist

--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
-import  cartReducer  from "./features/cart/cart.Slice.js";
+import cartReducer from "./features/cart/cart.Slice";
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, cartReducer);
 
 export const store = configureStore({
   reducer: {
-    cart: cartReducer
-  }
+    cart: persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // 👇 ignore redux-persist actions
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
